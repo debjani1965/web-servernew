f8592c98e111bb0f674026ae6b51602c2d6f1ed4
Debjani <shellysg.sengupta@gmail.com>
2021-03-28 12:16:20 +0530

    Web server - init commit

---
+++ public/js/custom.js
@@ -0,0 +1,29 @@
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const loadingMsg = document.getElementById('loadingMsg');
+const errorMsg = document.getElementById('error');
+const locationMsg = document.getElementById('location');
+const forecastMsg = document.getElementById('forecast');
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    locationMsg.textContent = '';
+    forecastMsg.textContent = '';
+    errorMsg.textContent = '';
+    loadingMsg.textContent = "loading ....";
+    const location = search.value;
+    
+    fetch('http://localhost:3000/weather?address="'+ location +'"').then((response) => {
+        response.json().then((data) => {
+            loadingMsg.textContent = '';
+            
+            if(data.error) {
+                errorMsg.textContent = data.error;
+            } else {
+                const {location, forecastData} = data;
+                console.log(location, forecastData)
+                locationMsg.textContent = location;
+                forecastMsg.textContent = forecastData;
+            }
+        })
+    });
+})
---
+++ src/app.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+//Initialize app
+const app = express();
+
+//Define paths for Express config
+const publicPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');
+
+//Set view engine
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Set static path
+app.use(express.static(publicPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        creater: 'Debjani'
+    });
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Weather App-help',
+        message: "This a help page for weather app",
+        creater: 'Debjani'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Debjani',
+        email: 'test@test.com',
+        creater: 'Debjani'
+    });
+});
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: "No address provided"
+        });
+    }
+    let address = req.query.address;
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({error})
+        }
+        console.log(latitude, longitude)
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error})
+            }
+            res.send({
+                location,
+                forecastData,
+                address
+            })
+        })
+    })
+    
+});
+
+app.get('*', (req, res) => {
+    res.render('404.hbs')
+});
+const port = process.env.PORT | 3000;
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`)
+})
